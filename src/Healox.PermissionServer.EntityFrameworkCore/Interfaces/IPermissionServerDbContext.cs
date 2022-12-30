93fb115501f018d7707b1970c78b441c1f6aa94e
Walid-Abdulrazik <walid.abdulrazik@gmail.com>
2022-12-30 14:39:42 +0200

    EF Basic Classes

---
+++ src/Healox.PermissionServer.EntityFrameworkCore/Interfaces/IPermissionServerDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Healox.PermissionServer.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Healox.PermissionServer.EntityFrameworkCore.Interfaces
+{
+    public interface IPermissionServerDbContext : IDisposable
+    {
+        public DbSet<IdentityRole> IdentityRoles { get; set; }
+
+        public DbSet<Permission> Permissions { get; set; }
+
+        public DbSet<Role> Roles { get; set; }
+
+        public DbSet<RoleMap> RoleMaps { get; set; }
+
+        public DbSet<RolePermission> RolePermissions { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<UserRole> UserRoles { get; set; }
+    }
+}
---
+++ src/Healox.PermissionServer.EntityFrameworkCore/Options/PermissionServerStoreOptions.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Healox.PermissionServer.EntityFrameworkCore.Options
+{
+    public class PermissionServerStoreOptions
+    {
+        /// <summary>
+        /// Callback to configure the EF DbContext.
+        /// </summary>
+        /// <value>
+        /// The configure database context.
+        /// </value>
+        public Action<DbContextOptionsBuilder>? PermissionServerDbContext { get; set; }
+
+        /// <summary>
+        /// Callback in DI resolve the EF DbContextOptions. If set, ConfigureDbContext will not be used.
+        /// </summary>
+        /// <value>
+        /// The configure database context.
+        /// </value>
+        public Action<IServiceProvider, DbContextOptionsBuilder>? ResolveDbContextOptions { get; set; }
+
+        /// <summary>
+        /// Gets or sets the default schema.
+        /// </summary>
+        /// <value>
+        /// The default schema.
+        /// </value>
+        public string? DefaultSchema { get; set; } = null;
+
+        /// <summary>
+        /// Gets or sets table configurations.
+        /// </summary>
+        /// <value>
+        /// The table resource.
+        /// </value>
+        public TableConfiguration IdentityRole { get; set; } = new TableConfiguration("IdentityRoles");
+        public TableConfiguration Permission { get; set; } = new TableConfiguration("Permissions");
+        public TableConfiguration Role { get; set; } = new TableConfiguration("Roles");
+        public TableConfiguration RoleMap { get; set; } = new TableConfiguration("RoleMaps");
+        public TableConfiguration RolePermission { get; set; } = new TableConfiguration("RolePermissions");
+        public TableConfiguration User { get; set; } = new TableConfiguration("Users");
+        public TableConfiguration UserRole { get; set; } = new TableConfiguration("UserRoles");
+    }
+}
